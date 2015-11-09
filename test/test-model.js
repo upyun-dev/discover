96b6c76c0c5431329c28d9d55ea3d32a2c418a0a
ran <abbshr@outlook.com>
2015-11-09 11:11:55 +0800

    [test/test-model.js] - 添加model模块部分测试用例

--- test/test-model.js
+++ test/test-model.js
@@ -11,17 +11,39 @@ var databaseCfg = {
 
 describe('Test lib/model.js', function () {
   Model = Model.init({ db: databaseCfg });
+  var model = Model({
+    tableName: 'test',
+    fields: [{
+      type: 'int',
+      unique: true,
+      name: 'test'
+    }],
+    indices: []
+  });
 
   context('when invoke Model with new or as a normal function', function () {
     it('should return a newModel Class', function () {
       var model = Model({
-        tableName: 'test'
+        tableName: 'test',
+        fields: [{
+          type: 'int',
+          unique: true,
+          name: 'test'
+        }],
+        indices: []
       });
       var modelWithNew = new Model({
         tableName: 'test'
       });
-
       model.$constructor.should.be.equal(modelWithNew.$constructor);
+    });
+  });
+
+  describe('create a NewModel instance', function () {
+    it('should be success', function () {
+      var CustomModel = model;
+      var newModel = new CustomModel;
+      newModel.should.be.ok();
     });
   });
 });
