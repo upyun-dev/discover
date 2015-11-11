189de18b0323a0f063da69038466d368345d314e
ran <abbshr@outlook.com>
2015-11-11 15:09:06 +0800

    [test] - 修改配置样例

--- test/conf/config.sample.js
+++ test/conf/config.sample.js
@@ -10,11 +10,12 @@ module.exports = {
     password: 'password',
     database: 'discover1'
   },
+  // for bad configurations test
   dupDatabase: {
     poolSize: 0,
     host: '127.0.0.1',
     user: 'root',
-    password: '',
+    password: 'password',
     database: 'discover2'
   }
 };
