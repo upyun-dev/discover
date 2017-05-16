8456f1056ebcc5f22731a09e0953fe765fdfd734
ran <abbshr@outlook.com>
2017-05-16 15:47:00 +0800

    修改 index

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-require('coffee-script/register');
-module.exports = require('./src/discover');
+let { accessSync, constants: { F_OK } } = require("fs");
+
+try {
+  accessSync("./lib", F_OK);
+  module.exports = require("./lib/discover");
+} catch (_) {
+  require('coffee-script/register');
+  module.exports = require("./src/discover");
+}
